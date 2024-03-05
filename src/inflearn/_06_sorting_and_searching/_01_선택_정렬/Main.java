1f1a0b8388d72349951429390112788a9afd6764
geunhokinn <geunhokinn@gmail.com>
2024-03-05 14:27:02 +0900

    fix: 괄호 위치 수정

--- src/inflearn/_06_sorting_and_searching/_01_선택_정렬/Main.java
+++ src/inflearn/_06_sorting_and_searching/_01_선택_정렬/Main.java
@@ -6,14 +6,14 @@ public class Main {
     public int[] solution(int a, int[] arr){
         for(int i=0; i<a-1; i++) {
             int idx = i;
-            for(int j=i+1; j<a; j++){
-                if(arr[idx]>arr[j]){
-                    idx=j;
+            for(int j=i+1; j<a; j++) {
+                if (arr[idx] > arr[j]) {
+                    idx = j;
                 }
-                int tmp = arr[i];
-                arr[i] = arr[idx];
-                arr[idx] = tmp;
             }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
         }
         return arr;
     }
