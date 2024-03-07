001beae0c59ba560d1f61817497c3cc54b9b5cd5
geunhokinn <geunhokinn@gmail.com>
2024-03-07 19:53:09 +0900

    dfs: 부분집합 구하기

---
+++ src/inflearn/_07_dfs_bfs_basic/_06_부분집합_구하기/Main.java
@@ -0,0 +1,28 @@
+package inflearn._07_dfs_bfs_basic._06_부분집합_구하기;
+
+public class Main {
+    static int n;
+    static int[] ch;
+    public void DFS(int L){
+        if(L==n+1){
+            StringBuilder sb = new StringBuilder();
+            for(int i=1; i<=n; i++) {
+                if(ch[i]==1) sb.append(i+" ");
+            }
+            if(sb.length()>0) System.out.println(sb);
+        }
+        else{
+            ch[L] = 1;
+            DFS(L+1);
+            ch[L] = 0;
+            DFS(L+1);
+        }
+    }
+
+    public static void main(String[] args){
+        Main m = new Main();
+        n = 3;
+        ch = new int[n+1];
+        m.DFS(1);
+    }
+}
