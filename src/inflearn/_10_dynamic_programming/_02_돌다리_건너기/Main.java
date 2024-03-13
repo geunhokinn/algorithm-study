a3f6747d3a1d43390996dac41f25762a7a967c02
geunhokinn <geunhokinn@gmail.com>
2024-03-13 09:15:32 +0900

    dp: 돌다리 건너기

---
+++ src/inflearn/_10_dynamic_programming/_02_돌다리_건너기/Main.java
@@ -0,0 +1,23 @@
+package inflearn._10_dynamic_programming._02_돌다리_건너기;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[] dy;
+    public int solution(int n){
+        dy[1] = 1;
+        dy[2] = 2;
+        for(int i=3; i<=n+1; i++){
+            dy[i] = dy[i-1] + dy[i-2];
+        }
+        return dy[n+1];
+    }
+
+    public static void main(String[] args){
+        Main m = new Main();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        dy = new int[n+2];
+        System.out.println(m.solution(n));
+    }
+}
