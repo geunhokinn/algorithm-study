8d9713862fa0f84c5353e7ef22e3823620712c7b
geunhokinn <gghh1220@gmail.com>
2024-04-11 10:30:00 +0900

    sorting: 수 정렬하기 2

---
+++ src/boj/part1/ch05_sorting/_2751_수_정렬하기_2/Main.java
@@ -0,0 +1,22 @@
+package boj.part1.ch05_sorting._2751_수_정렬하기_2;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i=0; i<n; i++) {
+            bw.write(arr[i]+"\n");
+        }
+        bw.flush();
+    }
+}
