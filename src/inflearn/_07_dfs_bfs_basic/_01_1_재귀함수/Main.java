2fe6330bc26e768e4a63a66c9394cdbbbbe56daf
geunhokinn <gghh1220@gmail.com>
2024-09-09 22:02:58 +0900

    DFS: 재귀함수

---
+++ src/inflearn/_07_dfs_bfs_basic/_01_1_재귀함수/Main.java
@@ -0,0 +1,16 @@
+package inflearn._07_dfs_bfs_basic._01_1_재귀함수;
+
+public class Main {
+    public void DFS(int n){
+        if (n == 0) return;
+        else {
+            DFS(n-1);
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void main(String[] args){
+        Main m = new Main();
+        m.DFS(3);
+    }
+}
