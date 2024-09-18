ac19ec59775e07e0b3cb87d7291c019f4dc8b92a
geunhokinn <gghh1220@gmail.com>
2024-09-18 19:09:26 +0900

    DFS: 조합 구하기

---
+++ src/inflearn/_08_dfs_bfs_advanced/_09_1_조합_구하기/Main.java
@@ -0,0 +1,28 @@
+package inflearn._08_dfs_bfs_advanced._09_1_조합_구하기;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[] combi;
+    static int n, m;
+    public void DFS(int L, int s){
+        if (L == m) {
+            for (int x : combi) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i=s; i<=n; i++) {
+                combi[L] = i;
+                DFS(L+1, i+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        combi = new int[m];
+        main.DFS(0, 1);
+    }
+}
