dd3bac3aff49880685cbf36ad2ac20c9d82681e3
geunhokinn <gghh1220@gmail.com>
2024-09-20 23:52:31 +0900

    Dp: 계단오르기

---
+++ src/inflearn/_10_dynamic_programming/_01_1_계단오르기/Main.java
@@ -0,0 +1,23 @@
+package inflearn._10_dynamic_programming._01_1_계단오르기;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[] dy;
+    public int solution(int n){
+        dy[1] = 1;
+        dy[2] = 2;
+        for(int i=3; i<=n; i++) {
+            dy[i] = dy[i-1] + dy[i-2];
+        }
+        return dy[n];
+    }
+
+    public static void main(String[] args){
+        Main m = new Main();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        dy = new int[n+1];
+        System.out.println(m.solution(n));
+    }
+}
