dbeaf9e99ad31e2431e661cc1be042861109da78
geunhokinn <gghh1220@gmail.com>
2024-09-26 23:49:22 +0900

    Queue: 응급실

---
+++ src/inflearn/_05_stack_queue/_08_1_응급실/Main.java
@@ -0,0 +1,55 @@
+package inflearn._05_stack_queue._08_1_응급실;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Person{
+
+    int id;
+    int priority;
+
+    public Person(int id, int priority){
+        this.id = id;
+        this.priority = priority;
+    }
+}
+
+public class Main {
+    public int solution(int a, int b, int[] arr) {
+        int ans = 0;
+        Queue<Person> q = new LinkedList<>();
+
+        for (int i = 0; i < a; i++) {
+            q.offer(new Person(i, arr[i]));
+        }
+
+        while (!q.isEmpty()) {
+            Person tmp = q.poll();
+            for (Person x : q) {
+                if (x.priority > tmp.priority) {
+                    q.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if (tmp != null) {
+                ans++;
+                if (tmp.id == b) return ans;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        Main m = new Main();
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int[] arr = new int[a];
+        for(int i=0; i<a; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(m.solution(a, b, arr));
+    }
+}
