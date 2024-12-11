88a9547c34363a2d48dea49d236f847626fe2c80
geunhokinn <gghh1220@gmail.com>
2024-12-11 16:34:41 +0900

    String: CamelCase

---
+++ src/hackerrank/string/Solution.java
@@ -0,0 +1,48 @@
+package hackerrank.string;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+        // Write your code here
+        int cnt = 1;
+        for(char ch : s.toCharArray()) {
+            if(Character.isUpperCase(ch)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
