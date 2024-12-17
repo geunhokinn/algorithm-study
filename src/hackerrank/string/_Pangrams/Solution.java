7518370eb732be3c3c087a4bcf59f8812e3d2901
geunhokinn <gghh1220@gmail.com>
2024-12-17 14:51:04 +0900

    String: Pangrams

---
+++ src/hackerrank/string/_Pangrams/Solution.java
@@ -0,0 +1,55 @@
+package hackerrank.string._Pangrams;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+        // Write your code here
+        Set<Character> set = new HashSet<>();
+        String newString = s.toLowerCase();
+
+        for(char ch : newString.toCharArray()) {
+            if('a'<=ch && ch<='z'){
+                set.add(ch);
+            }
+        }
+
+        if(set.size() == 26) {
+            return "pangram";
+        } else {
+            return "not pangram";
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
