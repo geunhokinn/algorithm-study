7ef5815f3bbbdef76a60842f4af0d9466b7670b4
geunhokinn <gghh1220@gmail.com>
2024-12-21 17:01:51 +0900

    Implementation: Subarray_Division

---
+++ src/hackerrank/implementation/_Subarray_Division/Solution.java
@@ -0,0 +1,77 @@
+package hackerrank.implementation._Subarray_Division;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<Integer> s, int d, int m) {
+        // Write your code here
+        int size = s.size();
+        int cnt = 0;
+
+        for(int i=0; i<size; i++) {
+            int sum=0;
+
+            for(int j=0; j<m; j++) {
+                int index = i+j;
+                if(index == size) break;
+                sum += s.get(index);
+            }
+
+            if (sum == d) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] sTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> s = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int sItem = Integer.parseInt(sTemp[i]);
+            s.add(sItem);
+        }
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
