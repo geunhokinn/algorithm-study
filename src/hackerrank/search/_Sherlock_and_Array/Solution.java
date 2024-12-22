5a4d74522f74d990e47bae2e5091cacb489d200b
geunhokinn <gghh1220@gmail.com>
2024-12-22 23:01:55 +0900

    Search: Sherlock and Array

---
+++ src/hackerrank/search/_Sherlock_and_Array/Solution.java
@@ -0,0 +1,73 @@
+package hackerrank.search._Sherlock_and_Array;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'balancedSums' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static String balancedSums(List<Integer> arr) {
+        // Write your code here
+        String ans = "NO";
+        int size = arr.size();
+        int totalSum = 0;
+
+        for(int i=0; i<size; i++) {
+            totalSum += arr.get(i);
+        }
+
+        int leftSum = 0;
+        for(int i=0; i<size; i++) {
+            int rightSum = totalSum - leftSum - arr.get(i);
+            if(leftSum==rightSum) {
+                ans = "YES";
+                break;
+            }
+            leftSum += arr.get(i);
+        }
+
+        return ans;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int TItr = 0; TItr < T; TItr++) {
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrTemp[i]);
+                arr.add(arrItem);
+            }
+
+            String result = Result.balancedSums(arr);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
