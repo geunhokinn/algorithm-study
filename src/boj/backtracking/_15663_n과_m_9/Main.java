abf6c7a2a4cac63a48dca94e0589420234496174
geunhokinn <gghh1220@gmail.com>
2025-02-19 17:09:48 +0900

    Backtracking: N과 M (9)

---
+++ src/boj/backtracking/_15663_n과_m_9/Main.java
@@ -0,0 +1,64 @@
+package boj.backtracking._15663_n과_m_9;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+
+    static int n, m;
+    static int[] arr, ans, ch;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> set = new HashSet<>();
+
+    public static void DFS(int L) {
+        if(L == m) {
+            StringBuilder tmp = new StringBuilder();
+            for(int i=0; i<m; i++) {
+                tmp.append(ans[i]).append(" ");
+            }
+            String tmpString = tmp.toString();
+            if(!set.contains(tmpString)) {
+                set.add(tmpString);
+                sb.append(tmpString).append("\n");
+            }
+        }else {
+            for(int i=0; i<n; i++) {
+                if(ch[i] == 0) {
+                    ch[i] = 1;
+                    ans[L] = arr[i];
+                    DFS(L+1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // 1. 주어진 배열을 받고 정렬한다
+        // 2. for문으로 DFS를 순회한다
+        // 3. 중복 순열이 아니므로 체크한다
+        // 4. 중복 되는 순열이 있으면 set으로 제거한다
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        ans = new int[m];
+        ch = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        DFS(0);
+
+        System.out.println(sb);
+    }
+}
